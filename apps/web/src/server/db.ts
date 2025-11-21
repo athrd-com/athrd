cc98d4c53224671e098624adfca776ba5ac8bdb8
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2025-11-20 22:38:35 -0800

    fix prisma build

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -1,12 +1,18 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 import { env } from "~/env";
 import { PrismaClient } from "../../generated/prisma";
 
-const createPrismaClient = () =>
-  new PrismaClient({
+const createPrismaClient = () => {
+  const pool = new pg.Pool({ connectionString: env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    datasourceUrl: env.DATABASE_URL!,
   });
+};
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;

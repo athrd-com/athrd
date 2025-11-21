d1654a2136c92fb3079007d4e61b8c85a0516431
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2025-11-20 22:28:11 -0800

    add postintall

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -5,7 +5,7 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: env.DATABASE_URL!,
   });
 
 const globalForPrisma = globalThis as unknown as {
